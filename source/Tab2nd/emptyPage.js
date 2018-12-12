5faf5d75ae9c8291514ede3144d63c66c0685981
Andy Su <andy-su@163.com>
2018-12-12 17:31:37 +0800

    add navigation title, and force it center from android platform

--- source/Tab2nd/emptyPage.js
+++ source/Tab2nd/emptyPage.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 
 export default class EmptyPage extends Component {
+
+    static navigationOptions = ({ navigation }) => ({
+        title: `${navigation.state.params.title}`,
+        headerTitleStyle: {
+            // color: 'green',
+            textAlign: 'center',
+            flex: 1,
+        },
+        headerStyle: {
+            // backgroundColor: 'white',
+            // borderColor: 'red',
+            // borderWidth: 1
+            // alignItems no effect on headerStyle
+            // alignItems: 'center',
+        },
+        // headerLeft: (<View></View>),
+        headerRight: (<View style={{ width: 40 }}></View>)
+    });
+
+
     render() {
         return (
             <View style={styles.container}></View>
