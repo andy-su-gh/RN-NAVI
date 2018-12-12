105356fdeee1fa65331b03cb58d3b2c19713c7dc
Andy Su <andy-su@163.com>
2018-12-12 17:31:56 +0800

    UI

--- source/Tab2nd/discovery.js
+++ source/Tab2nd/discovery.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text, Button, FlatList } from 'react-native';
+import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
 
 export default class DiscoveryScreen extends Component {
     separaterView = () => {
-        return <View style={{height: 10}}></View>
+        return <View style={{height: 5}}></View>
     }
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={styles.container}>
                 <Text>Discovery Screen</Text>
                 {this.separaterView()}
                 <Button 
                     title='FlatList Demo'
                     onPress={() => {
-                        this.props.navigation.navigate('FlatListDemo', {});
+                        this.props.navigation.push('FlatListDemo', {});
                     }}></Button>
                 {this.separaterView()}
                 <Button
                     title='Empty Page'
                     onPress={() => {
-                        this.props.navigation.push('EmptyPage', {});
+                        this.props.navigation.push('EmptyPage', { title: 'WHATEVER' });
                     }}></Button>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        // justifyContent: 'center',
+        marginHorizontal: 10,
+    },
+});
