ced59c2df92d7f078efe5e21d5671dcee074cca3
Andy Su <andy-su@163.com>
2018-12-12 16:36:35 +0800

    Add empty page

--- source/Tab2nd/discovery.js
+++ source/Tab2nd/discovery.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import { View, Text, Button, FlatList } from 'react-native';
 
 export default class DiscoveryScreen extends Component {
+    separaterView = () => {
+        return <View style={{height: 10}}></View>
+    }
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Discovery Screen</Text>
+                {this.separaterView()}
                 <Button 
                     title='FlatList Demo'
                     onPress={() => {
                         this.props.navigation.navigate('FlatListDemo', {});
+                    }}></Button>
+                {this.separaterView()}
+                <Button
+                    title='Empty Page'
+                    onPress={() => {
+                        this.props.navigation.push('EmptyPage', {});
                     }}></Button>
             </View>
         );
--- source/naviPortal.js
+++ source/naviPortal.js
@@ -19,6 +19,7 @@ import HomeScreen from './Tab1st/home';
 // Tab2nd
 import DiscoveryScreen from './Tab2nd/discovery';
 import FlatListDemo from './Tab2nd/flatListDemo';
+import EmptyPage from './Tab2nd/emptyPage';
 // Tab3rd
 import NewScreen from './Tab3rd/new';
 // Tab4th
@@ -57,6 +58,9 @@ const AllStack = {
   },
   FlatListDemo: {
     screen: FlatListDemo,
+  },
+  EmptyPage: {
+    screen: EmptyPage,
   },
 
   //Tab3rd
