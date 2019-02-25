236d0da55ebad8d0d13080efc4f66a351cb5287a
Andy_Su <andy_su@epam.com>
2019-02-25 11:24:02 +0800

    App icon Modal Dialog demo

---
+++ source/Tab3rd/modalDemo.js
@@ -0,0 +1,300 @@
+import React, { Component } from 'react';
+import {
+    Modal,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    Image,
+    KeyboardAvoidingView,
+    ScrollView,
+    StyleSheet
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class ModalDemo extends Component {
+
+    // 构造
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: '0',
+            name: '',
+            description: '',
+        }
+    }
+
+    static propTypes = {
+        // _dialogTitle: PropTypes.string, //标题
+        // _value: PropTypes.string, //value
+        // _dialogLeftBtnTitle: PropTypes.string, //左按键标题
+        // _dialogRightBtnTitle: PropTypes.string, //右按键标题
+        // _dialogLeftBtnAction: PropTypes.func.isRequired, //左点击方法
+        // _dialogRightBtnAction: PropTypes.func.isRequired, //右点击方法
+        // _dialogVisible: PropTypes.bool, //显示还是隐藏
+        // _allowNegative: PropTypes.bool, // 是否允许输入负数
+    }
+
+    static defaultProps = {
+        visible: false,
+        supportedOrientations: ['portrait', 'landscape'],
+        title: '修改数量',
+        leftBtnTitle: '取消',
+        rightBtnTitle: '确定',
+
+        // _allowNegative: true,
+        // _value: '0',
+    }
+
+    componentDidMount() {
+    }
+
+    componentWillUnmount() {
+    }
+
+    componentDidUpdate(prevProps) {
+        // if (this.props._defaultValue !== prevProps._defaultValue) {
+        //     this.setState({
+        //         value: this.props._defaultValue,
+        //     }, () => {
+        //         // console.log('NumberInputModal: ', this.props._defaultValue);
+        //     });
+        // }
+    }
+
+    onPressOk = () => {
+        // if (!this.state.name.trim()) {
+        //     alert('请输入一个合法的名称！');
+        //     return;
+        // } else {
+        this.props.rightBtnAction({
+            name: this.state.name,
+            description: this.state.description
+        });
+        // }
+    }
+
+    render() {
+        return (
+            <Modal
+                visible={this.props.visible}
+                transparent={true}
+                animationType='slide'
+                supportedOrientations={this.props.supportedOrientations}
+                onRequestClose={() => { }} //如果是Android设备 必须有此方法
+            >
+                <ScrollView contentContainerStyle={styles.container}>
+                        <View style={{ flex: 1 }}></View>
+                        <KeyboardAvoidingView style={{width: 280, height: '100%', alignItems: 'center', justifyContent: 'center'}} behavior='padding' enabled>
+                        <View style={styles.dialogVisualContainer}>
+                            <View style={styles.satdDialogTitleContainer} >
+                                <Text style={styles.dialogTitleContent} > {this.props.title} </Text>
+                            </View>
+                            <View style={[styles.satdDialogInfoMainArea]} >
+                                
+                                {/* <View style={{ flex: 1, backgroundColor: 'red' }}></View> */}
+                                {/* <ScrollView> */}
+                                <View style={styles.bcConfirmModalInputContainer} >
+                                    <TextInput
+                                        ref='numberInputBox'
+                                        autoFocus={true}
+                                        underlineColorAndroid={'transparent'}
+                                        placeholder={'名称'}
+                                        style={[styles.bcConfirmModalPasswordInput]}
+                                        onChangeText={(name) => { this.setState({ name }) }}
+                                    // onSubmitEditing={() => { this.onPressOk(); }}
+                                    />
+                                </View>
+                                {/* <View style={styles.bcConfirmModalInputContainer} >
+                                    <TextInput
+                                        ref='numberInputBox'
+                                        autoFocus={true}
+                                        underlineColorAndroid={'transparent'}
+                                        placeholder={'名称'}
+                                        style={[styles.bcConfirmModalPasswordInput]}
+                                        onChangeText={(name) => { this.setState({ name }) }}
+                                    // onSubmitEditing={() => { this.onPressOk(); }}
+                                    />
+                                </View>
+                                <View style={styles.bcConfirmModalInputContainer} >
+                                    <TextInput
+                                        ref='numberInputBox'
+                                        autoFocus={true}
+                                        underlineColorAndroid={'transparent'}
+                                        placeholder={'名称'}
+                                        style={[styles.bcConfirmModalPasswordInput]}
+                                        onChangeText={(name) => { this.setState({ name }) }}
+                                    // onSubmitEditing={() => { this.onPressOk(); }}
+                                    />
+                                </View>
+                                <View style={styles.bcConfirmModalInputContainer} >
+                                    <TextInput
+                                        ref='numberInputBox'
+                                        autoFocus={true}
+                                        underlineColorAndroid={'transparent'}
+                                        placeholder={'名称'}
+                                        style={[styles.bcConfirmModalPasswordInput]}
+                                        onChangeText={(name) => { this.setState({ name }) }}
+                                    // onSubmitEditing={() => { this.onPressOk(); }}
+                                    />
+                                </View> */}
+                                
+                                <View style={{ height: 5 }}></View>
+
+                                <View style={[styles.bcConfirmModalInputContainer, styles.satdDescriptionInputContainerHeight]} >
+
+                                    <TextInput
+                                        ref='numberInputBox'
+                                        autoFocus={true}
+                                        numberOfLines={4}
+                                        multiline={true}
+                                        underlineColorAndroid={'transparent'}
+                                        placeholder={'备注'}
+                                        style={[styles.bcConfirmModalPasswordInput, styles.satdDescriptionInputHeight]}
+                                        onChangeText={(description) => { this.setState({ description }) }}
+                                    // onSubmitEditing={() => { this.onPressOk(); }}
+                                    />
+                                </View>
+                                {/* </ScrollView> */}
+                            </View>
+
+                            <View style={styles.satdDialogBottomContainer} >
+                                <TouchableOpacity
+                                    style={styles.bcConfirmModalDialogBtnViewItem}
+                                    onPress={this.props.leftBtnAction} >
+
+                                    <Text style={styles.bcConfirmModalLeftButton} >  {this.props.leftBtnTitle} </Text>
+                                </TouchableOpacity>
+                                <View style={styles.bcConfirmModalDialogBtnLineVertical}></View>
+                                <TouchableOpacity
+                                    style={styles.bcConfirmModalDialogBtnViewItem}
+                                    onPress={this.onPressOk} >
+
+                                    <Text style={styles.bcConfirmModalRightButton} > {this.props.rightBtnTitle}  </Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                        </KeyboardAvoidingView>
+                        <View style={{ flex: 1 }}></View>
+                </ScrollView>
+            </Modal>
+        );
+    }
+}
+
+const colors = {
+    alertPrimary: 'rgb(102, 102, 102)', // #666666 rgb
+    titleSixth: 'rgb(74,144,226)',
+    lineSecondary: 'rgb(225,225,225)',
+    borderDefault: '#CED0CE',
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row', 
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(52,52,52,0.5)', 
+    },
+    dialogVisualContainer: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        width: 280,
+        height: 300,
+        flexDirection: 'column',
+    },
+    satdDialogTopContainer: {
+        flex: 1,
+        padding: 20,
+        paddingBottom: 10,
+    },
+    satdDialogBottomContainer: {
+        borderTopWidth: 1,
+        borderColor: colors.lineSecondary,
+        // backgroundColor: '#4444aa',
+        flexDirection: 'row',
+        height: 43,
+        width: '100%',
+    },
+    dialogTitleContent: {
+        textAlign: 'center',
+        fontSize: 18,
+        color: '#4A4A4A',
+    },
+    satdDialogInfoMainArea: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        // alignItems: 'center',
+        // backgroundColor: '#008888',
+        marginHorizontal: 20,
+        marginBottom: 10,
+    },
+    bcConfirmModalInputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderRadius: 5,
+        marginTop: 1,
+        // padding: padding.sm,
+        borderColor: colors.borderDefault,
+        borderTopWidth: 1,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderBottomWidth: 1,
+        width: '100%',
+        paddingTop: 3,
+        paddingBottom: 3,
+        // paddingHorizontal: padding.sm,
+        justifyContent: 'center',
+        height: 36,
+    },
+    bcConfirmModalPasswordInput: {
+        padding: 0,
+        // marginLeft: padding.sm,
+        // marginHorizontal: 10,
+        paddingHorizontal: 10,
+        width: '100%',
+        height: 30,
+        fontSize: 16,
+        // fontWeight: 'normal',
+        // flex: 1,
+        // backgroundColor: '#6611dd'
+    },
+    satdDescriptionInputContainerHeight: {
+        flex: 1,
+    },
+    satdDescriptionInputHeight: {
+        height: '100%',
+    },
+    satdDialogTitleContainer: {
+        height: 46,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // width: '100%',
+        // backgroundColor: '#099000',
+    },
+    bcConfirmModalDialogBtnViewItem: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: '#AAAAAA',
+        // borderBottomLeftRadius: 8,
+        // borderBottomRightRadius: 8,
+    },
+    bcConfirmModalLeftButton: {
+        fontSize: 17,
+        borderBottomLeftRadius: 8,
+        color: colors.alertPrimary,
+    },
+    bcConfirmModalRightButton: {
+        fontSize: 17,
+        borderBottomRightRadius: 8,
+        color: colors.titleSixth,
+    },
+    bcConfirmModalDialogBtnLineVertical: {
+        width: 1,
+        backgroundColor: colors.lineSecondary,
+    },
+});
--- source/Tab3rd/new.js
+++ source/Tab3rd/new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import ModalDialog from './modalDemo';
 
 export default class NewScreen extends Component {
     static navigationOptions = ({ navigation }) => ({
@@ -11,11 +12,96 @@ export default class NewScreen extends Component {
         },
     });
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            showModalDialog: false,
+            result: {name: 'NAME', description: 'DESCRIPTION'},
+            title: '这是对话框的标题',
+        }
+    }
+
+    showModalDialog = (status) => {
+        this.setState({
+            showModalDialog: status,
+        }, () => {
+            // alert(this.state.showModalDialog);
+        });
+    }
+
+    saveAsTemplate = (result) => {
+        this.setState({ result }, () => {
+            this.showModalDialog(false);
+        });
+    }
+
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={styles.container}>
                 {/* <Text>Create New Screen</Text> */}
+                <TouchableOpacity style={styles.buttonContainer}
+                    onPress={() => { this.showModalDialog(true) }}
+                >
+                    <Text>Modal Demo</Text>
+                </TouchableOpacity>
+                <View style={{height: 15}}></View>
+
+                <Text style={styles.resultInfoText}>Modal Dialog Reture Value</Text>
+                <View style={styles.resultContainer}>
+                    <Text style={styles.resultInfoText}>{`Name: `}
+                        <Text style={styles.resultValueText}>{this.state.result && this.state.result.name || ''}</Text>
+                    </Text>
+                    <Text style={styles.resultInfoText}>{'Description: '}
+                        <Text style={styles.resultValueText}>{this.state.result && this.state.result.description || ''}</Text>
+                    </Text>
+                </View>
+
+                <ModalDialog
+                    visible={this.state.showModalDialog}
+                    title={this.state.title}
+                    leftBtnTitle={'Cancel'}
+                    rightBtnTitle={'OK'}
+                    leftBtnAction={() => { this.showModalDialog(false); }}
+                    rightBtnAction={(result) => { this.saveAsTemplate(result); }}
+                    // // _allowNegative={false}
+                    // // supportedOrientations: ['portrait', 'portrait-upside-down', 'landscape', 'landscape-left', 'landscape-right']
+                    // supportedOrientations={['landscape']} 
+                />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center', 
+        // justifyContent: 'center',
+    },
+    buttonContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 0.5,
+        borderColor: 'orange',
+        borderRadius: 5,
+        height: 30,
+        width: '100%',
+    },
+    resultContainer: {
+        marginTop: 5,
+        width: '100%',
+        padding: 10,
+        borderWidth: 0.5,
+        borderColor: 'gray',
+        borderRadius: 5,
+    },
+    resultInfoText: {
+        color: 'gray',
+        // fontSize: 16,
+    },
+    resultValueText: {
+        color: 'black',
+        fontSize: 14,
+    }
+});
